74a5ccb344730562813dbf533ef001fedf66d6be
Tobias Mareco <tobiasmareco2002@gmail.com>
2023-07-09 20:49:01 +0000

    U05-5.9 i18n internaconalition for diferent lang

--- invoices/sapui5/webapp/controller/App.controller.js
+++ invoices/sapui5/webapp/controller/App.controller.js
@@ -1,23 +1,33 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    'invoices/sapui5/model/Models' //importing from model.
+    'invoices/sapui5/model/Models',
+    'sap/ui/model/resource/ResourceModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
     function (
         Controller,
 	MessageToast,
-	Models) {
+	Models,
+	ResourceModel) {
         'use strict'
         return Controller.extend("invoices.sapui5.controller.App", {
             onInit: function () {
+                //set data moderl for view
                 this.getView().setModel(Models.createRecipient())
+                //set i18n for view
+                let i18nModel = new ResourceModel({bundleName:"invoices.sapui5.i18n.i18n"})
+                this.getView().setModel(i18nModel,'i18n')
             },
             onSayHelloButton: function (oEvent) {
-                MessageToast.show('Hello from Message toast');
+                let objectBundle = this.getView().getModel("i18n").getResourceBundle();
+                let sRecipient = this.getView().getModel().getProperty('/recipient/name');
+                let textMessage = objectBundle.getText("helloMessage",[sRecipient]);
+                MessageToast.show(textMessage);
             }
         })
     })
