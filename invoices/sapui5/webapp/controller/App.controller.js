df6c2d1e3bcf9edeaf57a229b351908c0d55aef9
Tobias Mareco <tobiasmareco2002@gmail.com>
2023-07-09 18:55:07 +0000

    U05-5.6 Modules add firt module fMessageToast from sap.m

--- invoices/sapui5/webapp/controller/App.controller.js
+++ invoices/sapui5/webapp/controller/App.controller.js
@@ -1,16 +1,18 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast' // Modulos
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageToast} MessageToast
      */
     function (
-        Controller) {
+        Controller,
+        MessageToast) {
         'use strict'
         return Controller.extend("invoices.sapui5.controller.App", {
             onSayHelloButton: function (oEvent) {
-                console.log(oEvent)
-                alert('Hello from button in controller');
+                MessageToast.show('Hello from Message toast');
             }
         })
     })
