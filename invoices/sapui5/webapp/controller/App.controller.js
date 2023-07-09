8f948728f5f310fee7dd0ca7eb2f515e5059c2fe
Tobias Mareco <tobiasmareco2002@gmail.com>
2023-07-09 19:52:22 +0000

    u05-5.7 Modelos Json set models view

--- invoices/sapui5/webapp/controller/App.controller.js
+++ invoices/sapui5/webapp/controller/App.controller.js
@@ -1,16 +1,29 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast' // Modulos
+    'sap/m/MessageToast',
+    'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
     function (
         Controller,
-        MessageToast) {
+	MessageToast,
+	JSONModel) {
         'use strict'
         return Controller.extend("invoices.sapui5.controller.App", {
+            onInit: function () {
+                //Crear la instancia del modelo JSON.
+                let objectData = {
+                    recipient:{
+                        name:"World"
+                    }
+                }
+                let objectModel = new JSONModel(objectData);
+                this.getView().setModel(objectModel)
+            },
             onSayHelloButton: function (oEvent) {
                 MessageToast.show('Hello from Message toast');
             }
