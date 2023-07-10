08751c1844d6f50020e8b383026eff1a7e09bcaf
Tobias Mareco <tobiasmareco2002@gmail.com>
2023-07-10 17:16:30 +0000

    U05-5.13 Manifest file descriptor of the ui5 app

--- invoices/sapui5/webapp/controller/App.controller.js
+++ invoices/sapui5/webapp/controller/App.controller.js
@@ -1,27 +1,22 @@
-sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     * @param {typeof sap.m.MessageToast} MessageToast
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
-     */
-    function (
-        Controller,
-	MessageToast,
-	Models,
-	ResourceModel) {
-        'use strict'
-        return Controller.extend("invoices.sapui5.controller.App", {
-            onInit: function () {
-
-            },
-            onSayHelloButton: function (oEvent) {
-                let objectBundle = this.getView().getModel("i18n").getResourceBundle();
-                let sRecipient = this.getView().getModel().getProperty('/recipient/name');
-                let textMessage = objectBundle.getText("helloMessage",[sRecipient]);
-                MessageToast.show(textMessage);
-            }
-        })
-    })
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   * @param {typeof sap.m.MessageToast} MessageToast
+   * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+   */
+  function (Controller, MessageToast, Models, ResourceModel) {
+    "use strict";
+    return Controller.extend("invoices.sapui5.controller.App", {
+      onInit: function () {},
+      onSayHelloButton: function (oEvent) {
+        let objectBundle = this.getView().getModel("i18n").getResourceBundle();
+        let sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        let textMessage = objectBundle.getText("helloMessage", [sRecipient]);
+        MessageToast.show(textMessage);
+      },
+    });
+  }
+);
--- invoices/sapui5/webapp/index.js
+++ invoices/sapui5/webapp/index.js
@@ -1,17 +1,15 @@
 sap.ui.define(
-    [
-        "sap/ui/core/ComponentContainer"
-    ],
-    /**
-     * @param {typeof sap.ui.core.ComponentContainer} ComponentContainer
-     */
-    function (ComponentContainer) {
-        new ComponentContainer({
-            name:"invoices.sapui5",
-            settings:{
-                id:"sapui5"
-            },
-            async: true,
-        }).placeAt("root")
-    }
+  ["sap/ui/core/ComponentContainer"],
+  /**
+   * @param {typeof sap.ui.core.ComponentContainer} ComponentContainer
+   */
+  function (ComponentContainer) {
+    new ComponentContainer({
+      name: "invoices.sapui5",
+      settings: {
+        id: "sapui5",
+      },
+      async: true,
+    }).placeAt("root");
+  }
 );
