6591a61e94a99b28ab6ffa5c0f0487444e960314
Tobias Mareco <tobiasmareco2002@gmail.com>
2023-07-14 18:49:45 +0000

    U07-7.3 Dialogos add first dialog component

--- invoices/sapui5/webapp/controller/HelloPanel.controller.js
+++ invoices/sapui5/webapp/controller/HelloPanel.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    * @param {typeof sap.m.MessageToast} MessageToast
+   * @param {typeof sap.ui.core.Fragment} Fragment
    */
-  function (Controller, MessageToast) {
+  function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("invoices.sapui5.controller.HelloPanel", {
       onInit: function () {},
@@ -15,6 +16,21 @@ sap.ui.define(
           .getProperty("/recipient/name");
         let sMessage = objectBundle.getText("helloMessage", [sRecipient]);
         MessageToast.show(sMessage);
+      },
+      //"sap/ui/core/Fragment"
+      onOpenDialog: function () {
+        const objectView = this.getView();
+        if (!this.byId("Dialog")) {
+          Fragment.load({
+            id: objectView.getId(),
+            name: "invoices.sapui5.view.HelloDialog",
+          }).then(function (objectDialog) {
+            objectView.addDependent(objectDialog);
+            objectDialog.open();
+          });
+        } else {
+          this.byId("Dialog").open();
+        }
       },
     });
   }
